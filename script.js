d7d8895c9008e12f72525c7329e3f345ca758bae
SUMIT-IITPATNA <166421613+SUMIT-IITPATNA@users.noreply.github.com>
2024-06-14 13:16:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const loginButton = document.querySelector(".login");
+    loginButton.addEventListener("click", function() {
+        alert("Login button clicked!");
+     
+    });
+
+  
+    const signupButton = document.querySelector(".signup");
+    signupButton.addEventListener("click", function() {
+        alert("Signup button clicked!");
+        
+    });
+});
