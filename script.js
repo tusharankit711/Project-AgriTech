7808a1b668cb93c3d57f92f150af6aadb6793c47
SUMIT-IITPATNA <166421613+SUMIT-IITPATNA@users.noreply.github.com>
2024-06-20 10:46:22 +0530

    Update script.js
    i removed alert when we click on buttons

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loginButton = document.querySelector(".login");
     loginButton.addEventListener("click", function() {
-        alert("Login button clicked!");
+        
     });
 
     const signupButton = document.querySelector(".signup");
     signupButton.addEventListener("click", function() {
-        alert("Signup button clicked!");
+        
     });
 
     const languageButtons = document.querySelectorAll(".language-switcher button");
